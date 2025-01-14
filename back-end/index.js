eae512f549c43067070834ed7b2d23118a7be165
Akvilija <akvilija.skorupskaite199503@gmail.com>
2025-01-14 20:05:42 +0200

    added editing and deleting

--- back-end/index.js
+++ back-end/index.js
@@ -62,5 +62,54 @@ app.post('/cities', async (req, res) => {
     }
 })
 
+app.put('/cities/:id', async (req, res) => {
+    try {
+        const { name, population, location } = req.body
+        const { id } = req.params
+
+        const connection = await client.connect()
+        const result = await connection
+            .db('node-project')
+            .collection('cities')
+            .updateOne({ _id: ObjectId.createFromHexString(id) }, {
+                $set: {
+                    name,
+                    population,
+                    location
+                }
+            })
+
+            if (result.matchedCount === 0) {
+                return res.status(404).json({ error: 'City not found' });
+            }
+
+            await connection.close()
+
+        return res.send(result)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update city' })
+    }
+})
+
+app.delete('/cities/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const connection = await client.connect()
+        const result = await connection
+            .db('node-project')
+            .collection('cities')
+            .deleteOne({ _id: ObjectId.createFromHexString(id) })
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: 'City not found' })
+        }
+
+        await connection.close()
+        return res.send({ message: 'City deleted' })
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete city' })
+    }
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Server is running on port ${port}`))
